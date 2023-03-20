effbf16fec317609b54e0ff2162838eadfbbb981
Dmitry Boulytchev <dboulytchev@gmail.com>
2023-03-21 00:48:01 +0300

    ocanrun in WaterPuzzle

--- WaterPuzzle/WaterPuzzle.ml2mk.ml
+++ WaterPuzzle/WaterPuzzle.ml2mk.ml
@@ -1,5 +1,5 @@
-(* Natural numbers in Peano encoding *)
 open Peano
+open List
 
 let ( - ) a b = if a >= b then a - b else 0
 
@@ -10,26 +10,7 @@ let ( - ) a b = if a >= b then a - b else 0
    --- fill B
 *)
 type move = AB | BA | FillA | FillB
-
-(*
-Make this type definition compile
-type set = (nat, nat, nat, nat)
-*)
-
-(*
-    A set is a quadruple:
-       (capacity of A, capacity of B, contents of A, contents of B)
-*)
-
-(* set -> move -> set
-
-   Performs a given move for a given set if possible
-*)
-
-(* set -> move -> set
-
-   Performs a given move for a given set if possible
-*)
+                            
 let step (capA, capB, a, b) = function
 | FillA -> (capA, capB, capA, b)
 | FillB -> (capA, capB, a, capB)
@@ -41,10 +22,7 @@ let step (capA, capB, a, b) = function
 (* set -> move list -> set
    Performs a number of moves for a given set
 *)
-let rec eval set = function
-| []         -> set
-| m :: moves -> eval (step set m) moves
-
+let eval = fold_left step
 
 [@@@ocaml
 open GT
@@ -52,19 +30,17 @@ open OCanren
 open OCanren.Std
 open HO
      
-ocanren type answer = move list 
+ocanren type answer = move GT.list 
                     
 let _ = 
-  Printf.printf "Test!\n";
   Printf.printf "%s\n"
   @@ show (answer) 
   @@ Stdlib.List.hd
   @@ Stream.take ~n:1
-  @@ run q (fun q -> ocanren {
-         fresh a, b, c, d in
-           FO.eval (5, 3, 0, 0) q (a, b, c, d) &
-           {a == 1 | d == 1}
-         }) (fun rr -> rr#reify prj_exn_answer)
+  @@ ocanrun (q : answer) {fresh a, b, c, d in
+                           FO.eval (5, 3, 0, 0) q (a, b, c, d) &
+                           {a == 1 | d == 1}
+                          } -> q
 ]
           
 
